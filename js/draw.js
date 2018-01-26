75c4cdf6126b73334a104c2497ea4ac303d271ec
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-01-26 13:48:05 +0300

    feat(draw): basic draw shapes

--- js/draw.js
+++ js/draw.js
@@ -33,3 +33,76 @@ export class Size {
     return '{width: ' + this.width + ', height: ' + this.height + '}';
   }
 }
+
+export class Shape {
+  draw(context, options) {
+    if (options.fill) {
+      context.fill();
+    }
+    if (options.stroke) {
+      context.stroke();
+    }
+  }
+}
+
+export class Line extends Shape {
+  constructor(origin, terminal) {
+    super();
+    this.origin = origin;
+    this.terminal = terminal;
+  }
+
+  draw(context, options = { fill: false, stroke: true }) {
+    context.beginPath();
+    context.moveTo(this.origin.x, this.origin.y);
+    context.lineTo(this.terminal.x, this.terminal.y);
+    context.closePath();
+
+    super.draw(context, options);
+  }
+}
+
+export class Rectangle extends Shape {
+  constructor(center, size, options = { radius: 0 }) {
+    super();
+    this.center = center;
+    this.size = size;
+    this.options = options;
+  }
+
+  corner(corner) {
+    if (corner == Corner.LeftTop) {
+      return new Point(this.center.x - this.size.width/2, this.center.y - this.size.height/2);
+    } else if (corner == Corner.RightTop) {
+      return new Point(this.center.x + this.size.width/2, this.center.y - this.size.height/2);
+    } else if (corner == Corner.RightBottom) {
+      return new Point(this.center.x + this.size.width/2, this.center.y + this.size.height/2);
+    } else if (corner == Corner.LeftBottom) {
+      return new Point(this.center.x - this.size.width/2, this.center.y + this.size.height/2);
+    }
+
+    return null;
+  }
+
+  draw(context, options = { fill: true, stroke: true }) {
+    const x = this.center.x - this.size.width/2;
+    const y = this.center.y - this.size.height/2;
+    const width = this.size.width;
+    const height = this.size.height;
+    const radius = this.options.radius;
+
+    context.beginPath();
+    context.moveTo(x + radius, y);
+    context.lineTo(x + width - radius, y);
+    context.quadraticCurveTo(x + width, y, x + width, y + radius);
+    context.lineTo(x + width, y + height - radius);
+    context.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+    context.lineTo(x + radius, y + height);
+    context.quadraticCurveTo(x, y + height, x, y + height - radius);
+    context.lineTo(x, y + radius);
+    context.quadraticCurveTo(x, y, x + radius, y);
+    context.closePath();
+
+    super.draw(context, options);
+  }
+}
