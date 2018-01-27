fa41eb4978d0632c516e92d12b005f138104ba12
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-01-27 18:08:33 +0300

    feat(timeline): print names

--- js/timeline.js
+++ js/timeline.js
@@ -12,6 +12,7 @@ export class Timeline {
     for (let key in this.items) {
       const item = this.items[key];
 
+      const name = get(item, 'name');
       const x = get(item, 'position.x');
       const y = get(item, 'position.y');
       const width = get(item, 'size.width') || get(this.options, 'size.width');
@@ -19,12 +20,22 @@ export class Timeline {
       const radius = get(item, 'radius') || get(this.options, 'radius');
       const fill = get(item, 'color.fill') || get(this.options, 'color.fill');
       const stroke = get(item, 'color.stroke') || get(this.options, 'color.stroke');
+      const text = get(item, 'color.text') || get(this.options, 'color.text');
+      const font = get(item, 'text.font') || get(this.options, 'text.font');
+      const align = get(item, 'text.align') || get(this.options, 'text.align');
+      const baseline = get(item, 'text.baseline') || get(this.options, 'text.baseline');
 
       context.fillStyle = fill;
       context.strokeStyle = stroke;
+      context.font = font;
+      context.textAlign = align;
+      context.textBaseline = baseline;
 
       objects[key] = new Rectangle(new Point(x, y), new Size(width, height), { radius: radius });
       objects[key].draw(context);
+
+      context.fillStyle = text;
+      context.fillText(name, x, y);
     }
   }
 }
